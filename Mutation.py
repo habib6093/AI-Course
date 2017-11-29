766cae2bd835390bbc325ef234e141d852077875
Habibur Rahman <habibrahman6093@gmail.com>
2017-11-30 04:31:15 +0600

    Update Mutation.py

--- Mutation.py
+++ Mutation.py
@@ -1,199 +1,143 @@
-from random import randint 
-import math
+from random import randint
 import copy
 
+
 class Individual:
-    geneLength= None
-    fitness=None
-    gene=[]
-    
-    def __init__ (self,length=5,fit=0):
+    geneLength = None
+    fitness = None
+    gene = []
 
-            self.geneLength=length
-            self.fitness=fit
-            self.gene=[0]*length
-            
-            for i in range(self.geneLength):
-               self.gene[i]=randint(0,1)
+    def __init__(self, length, fit=0):
 
+        self.geneLength = length
+        self.fitness = fit
+        self.gene = [0] * length
+
+        for i in range(self.geneLength):
+            self.gene[i] = randint(0, 1)
 
     def calculateFitness(self):
-            self.fitness=0
-            for i in range(self.geneLength):
-                if(self.gene[i]==1):
-                    self.fitness+=1
+        self.fitness = 0
+        for i in range(self.geneLength):
+            if (self.gene[i] == 1):
+                self.fitness += 1
 
-            return self.fitness
-    
+        return self.fitness
 
-    
 
 class Population:
-    populationSize=None
-    individuals=[]
-    fittest=None
-    
-    def __init__ (self,size=5,indi=[],fit=0):
-        self.populationSize=size
-        self.fittest=fit 
-        self.individuals=indi
-        
+    populationSize = None
+    individuals = []
+    fittest = None
+
+    def __init__(self, size, indi=[], fit=0):
+        self.populationSize = size
+        self.fittest = fit
+        self.individuals = indi
 
 
-    def calculateFitness(self):
-            
-            for i in self.individuals:
-                i.calculateFitness()
 
-            self.individuals.sort(key=lambda x: x.fitness, reverse=False)
-                
+    def calculateFitness(self,temp):
+        for i in temp:
+            i.calculateFitness()
+
+        temp.sort(key=lambda x: x.fitness, reverse=False)
+
+        return temp   #temp contains array of objects
+
+
 
     def getFittestIndividual(self):
-            self.calculateFitness()
-            return self.individuals[-1]
-    
+        temp=self.calculateFitness(self.individuals)
+        self.individuals=temp
 
-    
+        return temp[-1]
+
+
 
     def getSecondFittestIndividual(self):
-        return self.individuals[-2]
-            
+        temp=self.calculateFitness(self.individuals)
+        return temp[-2]
+
 
 
     def getLeastFittestIndividual(self):
-        return self.individuals[0]
+        temp=self.calculateFitness(self.individuals)
+        return temp[0]
+
 
 
     def selection(self):
-        self.getFittestIndividual()
-   
-        
+        mostFittest=copy.deepcopy(self.getFittestIndividual())
+        secondMostFittest=copy.deepcopy(self.getSecondFittestIndividual())
+
+        temp=[mostFittest,secondMostFittest]
+        return temp
+
+
 
 
     def crossOver(self):
-        self.selection()
-        
-        swapPoint=randint(0,self.individuals[0].geneLength)
-        
-        temp=copy.copy(self.individuals[-2:])
+        swapPoint = randint(0, self.individuals[0].geneLength)
+
+        temp = self.selection()
 
         for i in range(swapPoint):
-           temp[0].gene[i],temp[1].gene[i] = temp[1].gene[i],temp[0].gene[i]
+            temp[0].gene[i], temp[1].gene[i] = temp[1].gene[i], temp[0].gene[i]
 
-        
-       
-     
-        return temp
-            
-            
+        self.getFittestIndividual()
+
+        return copy.copy(temp)
+
+
+
     def mutation(self):
-        temp=self.crossOver()
-        
-        randomPoint=randint(0,self.individuals[0].geneLength-1)
-      #  print("random is: ", randomPoint)
-        temp[0].gene[randomPoint]=1
+        temp = self.crossOver()
 
-        randomPoint=randint(0,self.individuals[0].geneLength-1)
-       # print("random is: ", randomPoint)
-        temp[1].gene[randomPoint]=1
+        randomPoint = randint(0, self.individuals[0].geneLength - 1)
+        temp[0].gene[randomPoint] = 1
 
-        self.selection()
+        randomPoint = randint(0, self.individuals[0].geneLength - 1)
+        temp[1].gene[randomPoint] = 1
+
+
+        self.getFittestIndividual()
 
         return temp
 
-    
+
+
 
     def getFittestOffspring(self):
 
-        temp=self.mutation()
+        temp=self.calculateFitness(self.mutation())
 
-        temp[0].fitness=0
-        temp[1].fitness=0
-        
-        for i in range(2):
-            for j in range(temp[0].geneLength):
-                if(j==1):
-                     temp[i].fitness+=1
-        
-        temp.sort(key=lambda x: x.fitness, reverse=False)
+        self.individuals[0] = temp[-1]
 
-        self.individuals[0]=temp[-1]
-
-        self.calculateFitness()
-
-        return self.individuals[-1].fitness
-        
-
-        
-
-
-x=Individual(5,0)
-y=Individual(5,0)
-z=Individual(5,0)
-
-a=Population(3,[x,y,z],-1)
-
-count=0
-while(a.getFittestOffspring()<5):
-
-  count+=1
-
-
-print("generation is: ",count)
+        return self.getFittestIndividual().fitness
 
 
 
 
 
+geneLength=5
 
-##            
-##    --get fittest individual--
-##        -calculate and compare fitness of each individuals
-##        -get the index of fittest individual
-##        -fittest=fitness value of the highest individual
-##    -return fittest individual
-##    
-##    --get second most fittest individual--
-##    --get index of least fittest individual--
-##
-##--Selection--
-##    --select the most fittest individual--
-##    --seelect the second most fittest individual--
-##
-##--crossover--
-##    -select a random crossover point
-##    -swap values among parents-
-##
-##--mutation--
-##    -select a random mutation point
-##    -flip values at the muation point
-##
-##--get fittest offspring--
-##
-##--add fittest offspring-- [replacce least fittest individual from most fittest offspring]
-##    -update fitness value of offspring
-##    -get index of least fit individual
-##    -replace least fittest individual with most fittest offspring
-##
-##--main--
-##    - Population population=
-##    - Individual fittest=
-##    - Individual secondFittest=
-##    - generationCount=
-##
-##    --initialize population--
-##    --calculate fitness of each individual--
-##    --loop--
-##        -while population gets an individual with maximum fitness-
-##            - do selection
-##            - do crossover
-##            - do mudation 
-##            - add fittest offspring to population
-##            - calculate new fitness value
-##    
-##    
-##
-##        
-##        
-##    
-##    
+x = Individual(geneLength)
+y = Individual(geneLength)
+z = Individual(geneLength)
+
+
+populationSize=3
+
+a = Population(populationSize, [x, y, z])
+
+count = 1
+while (a.getFittestOffspring() < geneLength):
+    count += 1
+
+print(count," Generations are needed to find the fully fittest individual" )
+
+
+
+
+
